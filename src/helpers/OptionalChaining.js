26c975939743ad2f7d80ebda413677cfba929546
kanwal019 <kanwal019@gmail.com>
2025-12-19 00:34:37 +0530

    ✨Added an example of optional chaining with nullish coalescing

---
+++ src/helpers/OptionalChaining.js
@@ -0,0 +1,12 @@
+import { getBook } from "./books.js";
+
+const getTotalReviewCount = (book) => {
+  // Using optional chaining and nullish coalescing to safely access nested properties
+  const libraryThingCount = book.reviews?.librarything?.reviewsCount ?? 0;
+  const goodreadsCount = book.reviews?.goodreads?.reviewsCount ?? 0;
+  return libraryThingCount + goodreadsCount;
+};
+
+console.log(getTotalReviewCount(getBook(1)));
+console.log(getTotalReviewCount(getBook(2)));
+console.log(getTotalReviewCount(getBook(3)));

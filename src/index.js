1f4e068bbb2bb19417afeec5b4e26676de56a4df
kanwal019 <kanwal019@gmail.com>
2025-12-26 21:17:38 +0530

    💬Added extra text for the pizza

--- src/index.js
+++ src/index.js
@@ -73,6 +73,9 @@ function Menu() {
   return (
     <main className="menu">
       <h2>Our Menu</h2>
+      <p>
+        Authentic Italian cuisine. 6 creative dishes to choose from. All from our stone oven, all organic, all delicious.
+      </p>
       {pizzaLength > 0 ? (
         <ul className="pizzas">
           {pizzas.map((pizza) => (

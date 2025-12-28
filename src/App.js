77afbc54d4c9e86fd2e21c6efbb743d5bf84aaa6
kanwal019 <kanwal019@gmail.com>
2025-12-29 03:26:02 +0530

    ♻️Updated the App component with the steps frame

--- src/App.js
+++ src/App.js
@@ -1,3 +1,21 @@
 export default function App() {
-  return <div>Hello React!</div>;
+  return (
+    <div className="steps">
+      <div className="numbers">
+        <div className="number active">1</div>
+        <div className="number">2</div>
+        <div className="number">3</div>
+        <div className="number">4</div>
+      </div>
+      <p className="message">Hello React!</p>
+      <div className="buttons">
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Previous
+        </button>
+        <button style={{ backgroundColor: "#7950f2", color: "#fff" }}>
+          Next
+        </button>
+      </div>
+    </div>
+  );
 }

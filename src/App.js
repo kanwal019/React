e1d80a2830cfd9438725cd3ea9118eb08d36f0c4
kanwal019 <kanwal019@gmail.com>
2026-01-02 22:18:07 +0530

    ✨Using the state for conditional rendering

--- src/App.js
+++ src/App.js
@@ -9,7 +9,8 @@ const messages = [
 
 export default function App() {
   const [step, setStep] = useState(1);
-  const [test] = useState({ name: "Test" });
+  const [isOpen, setIsOpen] = useState(true);
+  // const [test] = useState({ name: "Test" });
 
   function handlePrevious() {
     if (step > 1) setStep(step - 1);
@@ -17,36 +18,40 @@ export default function App() {
 
   function handleNext() {
     if (step < messages.length) setStep(step + 1);
-    test.name = "Changed"; // This is a bad example of mutating state directly
+    // test.name = "Changed"; // This is a bad example of mutating state directly
   }
 
   return (
-    <div className="steps">
-      <div className="numbers">
-        <div className={step >= 1 ? "active" : ""}>1</div>
-        <div className={step >= 2 ? "active" : ""}>2</div>
-        <div className={step >= 3 ? "active" : ""}>3</div>
-        <div className={step >= 4 ? "active" : ""}>4</div>
-      </div>
-      <p className="message">
-        Step {step}: {messages[step - 1]} {test.name}
-      </p>
-      <div className="buttons">
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handlePrevious}
-          onMouseEnter={() => console.debug("Enter Previous button")}
-        >
-          Previous
-        </button>
-        <button
-          style={{ backgroundColor: "#7950f2", color: "#fff" }}
-          onClick={handleNext}
-          onMouseEnter={() => console.debug("Enter Next button")}
-        >
-          Next
-        </button>
-      </div>
+    <div>
+      {isOpen && (
+        <div className="steps">
+          <div className="numbers">
+            <div className={step >= 1 ? "active" : ""}>1</div>
+            <div className={step >= 2 ? "active" : ""}>2</div>
+            <div className={step >= 3 ? "active" : ""}>3</div>
+            <div className={step >= 4 ? "active" : ""}>4</div>
+          </div>
+          <p className="message">
+            Step {step}: {messages[step - 1]} {/*{test.name}*/}
+          </p>
+          <div className="buttons">
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+              onMouseEnter={() => console.debug("Enter Previous button")}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+              onMouseEnter={() => console.debug("Enter Next button")}
+            >
+              Next
+            </button>
+          </div>
+        </div>
+      )}
     </div>
   );
 }

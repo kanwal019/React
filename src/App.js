d3e1f94351f21f0cb6ca9dee48392753769c979d
kanwal019 <kanwal019@gmail.com>
2026-01-22 23:54:04 +0530

    ♻️Refactored the code to update the counter for days

--- src/App.js
+++ src/App.js
@@ -1,50 +1,37 @@
 import { useState } from "react";
 
 export default function App() {
+  return (<Counter />);
+}
+
+function Counter() {
   const [step, setStep] = useState(1);
   const [count, setCount] = useState(0);
-  const [date, setDate] = useState(new Date());
-
-  function addSteps() {
-    setStep((s) => s + 1);
-  }
-  function subtractSteps() {
-    setStep((s) => s - 1);
-  }
-
-  function addCount() {
-    setCount((c) => c + step);
-    updateDate();
-  }
-
-  function subtractCount() {
-    setCount((c) => c - step);
-    updateDate();
-  }
-
-  function updateDate() {
-    setDate(() => new Date());
-    setDate((d) => {
-      const newDate = new Date(d);
-      newDate.setDate(d.getDate() + count);
-      return newDate;
-    });
-    console.log("Date updated -", date);
-  }
+  const date = new Date("January 22, 2026");
+  date.setDate(date.getDate() + count);
 
   return (
     <div style={{ fontFamily: "sans-serif", fontSize: "24px", textAlign: "center", marginTop: "50px" }}>
       <div style={{ marginTop: "10px" }}>
-        <button onClick={subtractSteps}>-</button>
+        <button onClick={() => setStep((s) => s - 1)}>-</button>
         <span> Step: {step} </span>
-        <button onClick={addSteps}>+</button>
+        <button onClick={() => setStep((s) => s + 1)}>+</button>
       </div>
       <div style={{ marginTop: "10px" }}>
-        <button onClick={subtractCount}>-</button>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
         <span> Count: {count} </span>
-        <button onClick={addCount}>+</button>
+        <button onClick={() => setCount((c) => c + step)}>+</button>
       </div>
-      <div style={{ marginTop: "10px" }}>{count} days from now is {date.toDateString()}</div>
+      <div style={{ marginTop: "10px" }}>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
     </div>
   );
 }

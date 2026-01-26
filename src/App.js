623b44b94eac4453661f879b608d75b08407504d
kanwal019 <kanwal019@gmail.com>
2026-01-27 00:03:42 +0530

    ✨Packing list displays the list from the seeded data

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 const initialItems = [
   { id: 1, description: "Passports", quantity: 2, packed: false },
-  { id: 2, description: "Socks", quantity: 12, packed: false },
+  { id: 2, description: "Socks", quantity: 12, packed: true },
+  { id: 3, description: "Charger", quantity: 1, packed: false },
 ];
 
 export default function App() {
@@ -19,19 +20,42 @@ function Logo() {
 }
 
 function Form() {
-  return <div className="add-form">
-    <h3>What do you need for your 😍 trip?</h3>
-  </div>
+  return (
+    <div className="add-form">
+      <h3>What do you need for your 😍 trip?</h3>
+    </div>
+  );
 }
 
 function PackingList() {
-  return <div className="list">
-    <h3>Packing List</h3>
-  </div>
+  return (
+    <div className="list">
+      <ul>
+        {
+          initialItems.map(item => (
+            <Item item={item} />
+          ))
+        }
+      </ul>
+    </div>
+  );
+}
+
+function Item({ item }) {
+  return (
+    <li>
+      <span style={item.packed ? { textDecoration: "line-through" } : {}}>
+        {item.quantity} {item.description}
+      </span>
+      <button>❌</button>
+    </li>
+  );
 }
 
 function Stats() {
-  return <footer className="stats">
-    <em>💼You have 0 items on your list, you have already packed 0 (0%)</em>
-  </footer>
+  return (
+    <footer className="stats">
+      <em>💼You have 0 items on your list, you have already packed 0 (0%)</em>
+    </footer>
+  );
 }

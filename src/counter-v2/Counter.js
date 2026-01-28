a17fca2114434b277bc6c3fdf0cda61745efca35
kanwal019 <kanwal019@gmail.com>
2026-01-29 02:34:34 +0530

    🎨Moved the counter component to a seperate module

---
+++ src/counter-v2/Counter.js
@@ -0,0 +1,56 @@
+import { useState } from "react";
+
+export default function App() {
+  return (<Counter />);
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+  const date = new Date("January 29, 2026");
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <div style={{ fontFamily: "sans-serif", fontSize: "24px", textAlign: "center", marginTop: "50px" }}>
+      <div style={{ marginTop: "10px" }}>
+        <input
+          type="range"
+          min="1"
+          max="10"
+          value={step}
+          onChange={(e) => setStep(Number(e.target.value))}
+        />
+        <span> Step: {step} </span>
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(Number(e.target.value))}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <div style={{ marginTop: "10px" }}>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+              ? `${count} days from today is `
+              : `${Math.abs(count)} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </div>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </div>
+  );
+}

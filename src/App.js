7be70b50dfb2db79f67ba8a12d4b6348ba9f261a
kanwal019 <kanwal019@gmail.com>
2026-02-03 00:23:06 +0530

    ✨Displayed total packed items and their percentage

--- src/App.js
+++ src/App.js
@@ -125,10 +125,13 @@ function Item({ item, onDeleteItem: deleteItem, onToggleItem: toggleItem }) {
 }
 
 function Stats({ items }) {
-    const numItems = items.length;
+  const numItems = items.length;
+  const numPacked = items.filter(item => item.packed).length;
+  const percentage = numItems === 0 ? 0 : Math.round((numPacked / numItems) * 100);
+  
   return (
     <footer className="stats">
-      <em>💼You have {numItems} items on your list, you have already packed 0 (0%)</em>
+      <em>💼You have {numItems} items on your list, you have already packed {numPacked} ({percentage}%)</em>
     </footer>
   );
 }

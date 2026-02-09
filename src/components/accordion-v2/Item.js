f10070ee4c4719ac361254b2edeab8a0548d0426
kanwal019 <kanwal019@gmail.com>
2026-02-10 01:59:56 +0530

    ♻️Refactored Accordion and Item components to pass title and children as props

--- src/components/accordion-v2/Item.js
+++ src/components/accordion-v2/Item.js
@@ -1,5 +1,5 @@
 import React from "react";
-export default function Item({ currOpen, onOpen, faq, index }) {
+export default function Item({ currOpen, onOpen, title, index, children }) {
     const isOpen = currOpen === index;
     return (
         <div
@@ -8,9 +8,9 @@ export default function Item({ currOpen, onOpen, faq, index }) {
             onClick={() => onOpen(index)}
         >
             <p className="number">{index < 9 ? `0${index + 1}` : index + 1}</p>
-            <p className="title">{faq.title}</p>
+            <p className="title">{title}</p>
             <p className="icon">{isOpen ? "-" : "+"}</p>
-            {isOpen && <div className="content-box">{faq.text}</div>}
+            {isOpen && <div className="content-box">{children}</div>}
         </div>
     );
 }

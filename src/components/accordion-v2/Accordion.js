4467a66e38417339c1f4e61f99713950d6ff8251
kanwal019 <kanwal019@gmail.com>
2026-02-10 01:54:07 +0530

    ♻️Refactored Accordion and Item components to manage open state through props

--- src/components/accordion-v2/Accordion.js
+++ src/components/accordion-v2/Accordion.js
@@ -1,11 +1,19 @@
+import React from 'react';
 import Item from './Item';
 
 export default function Accordion({ faqs }) {
+    const [currentOpenIndex, setCurrentOpenIndex] = React.useState(null);
     return (
         <div className="accordion">
             {
                 faqs.map((faq, i) => (
-                    <Item faq={faq} index={i} key={i} />
+                    <Item
+                        currOpen={currentOpenIndex}
+                        onOpen={setCurrentOpenIndex}
+                        faq={faq}
+                        index={i}
+                        key={i}
+                    />
                 ))
             }
         </div>
--- src/components/accordion-v2/Item.js
+++ src/components/accordion-v2/Item.js
@@ -1,11 +1,11 @@
 import React from "react";
-export default function Item({ faq, index }) {
-    const [isOpen, setIsOpen] = React.useState(false);
+export default function Item({ currOpen, onOpen, faq, index }) {
+    const isOpen = currOpen === index;
     return (
         <div
             className={`item ${isOpen ? "open" : ""}`}
             key={index}
-            onClick={() => setIsOpen(!isOpen)}
+            onClick={() => onOpen(index)}
         >
             <p className="number">{index < 9 ? `0${index + 1}` : index + 1}</p>
             <p className="title">{faq.title}</p>

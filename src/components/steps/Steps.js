d48d70e54786b7537f41df5cf3092c3289cda112
kanwal019 <kanwal019@gmail.com>
2026-02-09 23:38:12 +0530

    ♻️Refactored Steps component into its own file and updated App component to utilize it

---
+++ src/components/steps/Steps.js
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { messages } from "./App";
+
+export function Steps() {
+    const [step, setStep] = useState(1);
+    const [isOpen, setIsOpen] = useState(true);
+    // const [test] = useState({ name: "Test" });
+    function handlePrevious() {
+        if (step > 1) setStep((s) => s - 1);
+    }
+
+    function handleNext() {
+        if (step < messages.length) {
+            setStep((s) => s + 1);
+            // setStep(step + 1); // This will only increment by 1 due to how state updates are batched
+            // setStep((s) => s + 1); // This will only increment by 2 because React batches state updates
+        }
+        // test.name = "Changed"; // This is a bad example of mutating state directly
+    }
+
+    return (
+        <div>
+            <button className="close" onClick={() => setIsOpen((is) => !is)}>
+                &times;
+            </button>
+            {isOpen && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={step >= 1 ? "active" : ""}>1</div>
+                        <div className={step >= 2 ? "active" : ""}>2</div>
+                        <div className={step >= 3 ? "active" : ""}>3</div>
+                        <div className={step >= 4 ? "active" : ""}>4</div>
+                    </div>
+                    <p className="message">
+                        Step {step}: {messages[step - 1]} {/*{test.name}*/}
+                    </p>
+                    <div className="buttons">
+                        <button
+                            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                            onClick={handlePrevious}
+                            onMouseEnter={() => console.debug("Enter Previous button")}
+                        >
+                            Previous
+                        </button>
+                        <button
+                            style={{ backgroundColor: "#7950f2", color: "#fff" }}
+                            onClick={handleNext}
+                            onMouseEnter={() => console.debug("Enter Next button")}
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            )}
+        </div>
+    );
+}

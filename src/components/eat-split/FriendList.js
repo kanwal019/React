ff289d90c1aa72f5cab065b3e078318faebacd9f
kanwal019 <kanwal019@gmail.com>
2026-02-14 03:11:26 +0530

    ♻️Refactored friend list and friend component for rendering individual friends

--- src/components/eat-split/FriendList.js
+++ src/components/eat-split/FriendList.js
@@ -1,22 +1,11 @@
-export function FriendList({ friends }) {
+import Friend from "./Friend";
+
+export default function FriendList({ friends }) {
     return (
         <ul>
             {friends.map((friend) => (
                 <Friend key={friend.id} friend={friend} />
             ))}
         </ul>
-    );
-}
-
-function Friend({ friend }) {
-    return (
-        <li key={friend.id}>
-            <img src={friend.image} alt={friend.name} />
-            <h3>{friend.name}</h3>
-            {
-                friend.balance < 0 ?
-                    <p className="red">You owe {friend.name} ${Math.abs(friend.balance)}</p>
-                    : friend.balance > 0 ? <p className="green">You are owed ${friend.balance} by {friend.name}</p> : <p className="neutral">You and {friend.name} are even</p>}
-        </li>
     );
 }

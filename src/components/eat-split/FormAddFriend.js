88f195ac54dd261e2287e09968cfac86ee7c2b97
kanwal019 <kanwal019@gmail.com>
2026-02-15 23:44:08 +0530

    ✨Implemented friend addition functionality and updated FormAddFriend component

--- src/components/eat-split/FormAddFriend.js
+++ src/components/eat-split/FormAddFriend.js
@@ -1,13 +1,32 @@
 import Button from "./Button";
+import { useState } from "react";
 
-export default function FormAddFriend() {
+export default function FormAddFriend({ onAddFriend }) {
+    const [name, setName] = useState("");
+    const [image, setImage] = useState("https://i.pravatar.cc/48");
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        if (!name || !image) return;
+        const id = crypto.randomUUID();
+        const newFriend = {
+            id,
+            name,
+            image: `${image}?=${id}`,
+            balance: 0,
+        };
+        onAddFriend(newFriend);
+        setName("");
+        setImage("https://i.pravatar.cc/48");
+    }
+
     return (
-        <form className="form-add-friend">
+        <form className="form-add-friend" onSubmit={handleSubmit}>
             <label>👤 Name</label>
-            <input type="text" />
+            <input type="text" value={name} onChange={(e) => setName(e.target.value)} />
             <label>📷 Image URL</label>
-            <input type="text" />
-            <Button onClick={() => console.log("Adding new friend")}>Add</Button>
+            <input type="text" value={image} onChange={(e) => setImage(e.target.value)} />
+            <Button type="submit">Add</Button>
         </form>
     );
 }

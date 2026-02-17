6e461c0cbba9403d1fae27e0d2541c523d0c57f9
kanwal019 <kanwal019@gmail.com>
2026-02-18 01:41:35 +0530

    💩Lifted the movies state up to use it in multiple components

--- src/components/use-popcorn/NavBar.js
+++ src/components/use-popcorn/NavBar.js
@@ -2,12 +2,12 @@ import { Logo } from "./Logo";
 import { NumResults } from "./NumResults";
 import { Search } from "./Search";
 
-export function NavBar() {
+export function NavBar({ movies }) {
     return (
         <nav className="nav-bar">
             <Logo />
             <Search />
-            <NumResults />
+            <NumResults movies={movies} />
         </nav>
     );
 }

f7d01c6b863962c7a7c292c5fa1879b396298a57
kanwal019 <kanwal019@gmail.com>
2026-02-18 01:06:26 +0530

    ♻️Refactored main component into list box and watched box components

---
+++ src/components/use-popcorn/ListBox.js
@@ -0,0 +1,43 @@
+import { useState } from "react";
+import { tempMovieData } from "./App";
+
+export function ListBox() {
+    const [isOpen1, setIsOpen1] = useState(true);
+    return (
+        <div className="box">
+            <button
+                className="btn-toggle"
+                onClick={() => setIsOpen1((open) => !open)}
+            >
+                {isOpen1 ? "–" : "+"}
+            </button>
+            {isOpen1 && <MovieList />}
+        </div>
+    );
+}
+
+function MovieList() {
+    const [movies, setMovies] = useState(tempMovieData);
+    return (
+        <ul className="list">
+            {movies?.map((movie) => (
+                <Movie key={movie.imdbID} movie={movie} />
+            ))}
+        </ul>
+    );
+}
+
+function Movie({ movie }) {
+    return (
+        <li key={movie.imdbID}>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+            <h3>{movie.Title}</h3>
+            <div>
+                <p>
+                    <span>🗓</span>
+                    <span>{movie.Year}</span>
+                </p>
+            </div>
+        </li>
+    );
+}

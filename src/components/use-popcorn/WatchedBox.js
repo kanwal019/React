c0cf3798d660d7435e735318cbb9d5deccfb51e9
kanwal019 <kanwal019@gmail.com>
2026-02-18 01:21:13 +0530

    ♻️Decomposed watched box component into multiple components

--- src/components/use-popcorn/WatchedBox.js
+++ src/components/use-popcorn/WatchedBox.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
-import { tempWatchedData, average } from "./App";
+import { tempWatchedData } from "./App";
+import { WatchedSummary } from "./WatchedSummary";
+import { WatchedMoviesList } from "./WatchedMoviesList";
 
 export function WatchedBox() {
     const [watched, setWatched] = useState(tempWatchedData);
     const [isOpen2, setIsOpen2] = useState(true);
-
-    const avgImdbRating = average(watched.map((movie) => movie.imdbRating));
-    const avgUserRating = average(watched.map((movie) => movie.userRating));
-    const avgRuntime = average(watched.map((movie) => movie.runtime));
 
     return (
         <div className="box">
@@ -19,50 +17,8 @@ export function WatchedBox() {
             </button>
             {isOpen2 && (
                 <>
-                    <div className="summary">
-                        <h2>Movies you watched</h2>
-                        <div>
-                            <p>
-                                <span>#️⃣</span>
-                                <span>{watched.length} movies</span>
-                            </p>
-                            <p>
-                                <span>⭐️</span>
-                                <span>{avgImdbRating}</span>
-                            </p>
-                            <p>
-                                <span>🌟</span>
-                                <span>{avgUserRating}</span>
-                            </p>
-                            <p>
-                                <span>⏳</span>
-                                <span>{avgRuntime} min</span>
-                            </p>
-                        </div>
-                    </div>
-
-                    <ul className="list">
-                        {watched.map((movie) => (
-                            <li key={movie.imdbID}>
-                                <img src={movie.Poster} alt={`${movie.Title} poster`} />
-                                <h3>{movie.Title}</h3>
-                                <div>
-                                    <p>
-                                        <span>⭐️</span>
-                                        <span>{movie.imdbRating}</span>
-                                    </p>
-                                    <p>
-                                        <span>🌟</span>
-                                        <span>{movie.userRating}</span>
-                                    </p>
-                                    <p>
-                                        <span>⏳</span>
-                                        <span>{movie.runtime} min</span>
-                                    </p>
-                                </div>
-                            </li>
-                        ))}
-                    </ul>
+                    <WatchedSummary watched={watched} />
+                    <WatchedMoviesList watched={watched} />
                 </>
             )}
         </div>
